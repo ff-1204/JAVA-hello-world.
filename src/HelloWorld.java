86dcf94fde6a4f526ca29f15680a3428d3a71c8c
French-Fries-1204 <dorimhan@kakao.com>
2021-07-03 19:25:06 +0900

    첫 커밋

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World 한글!!");
+		System.out.println("스터디 화이팅ㅇ!");
+
+	}
+}
